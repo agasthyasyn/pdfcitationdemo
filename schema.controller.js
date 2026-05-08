e31c71985ac811517532a160efa4b20345793227
agasthyasyn <agasthya.c@synergixserv.com>
2026-05-08 21:39:26 +0530

    Update schema.controller.js

--- schema.controller.js
+++ schema.controller.js
@@ -1,1 +1,21 @@
+import { db, doc, getDoc } from "./firebase.client.js";
 
+export async function loadTemplateController() {
+  const ref = doc(db, "templateSchemas", "generic_template_alignment");
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    throw new Error("Firebase template controller not found: generic_template_alignment");
+  }
+
+  const controller = snap.data();
+
+  if (!controller.active) {
+    throw new Error("Firebase template controller is inactive.");
+  }
+
+  return {
+    id: snap.id,
+    ...controller
+  };
+}
